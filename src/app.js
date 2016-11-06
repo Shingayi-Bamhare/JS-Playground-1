4754f06a65e79712a1a8959b4b83d2bf06523e9a
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2016-11-06 19:58:14 +0000

    add a style loader to playground

--- src/app.js
+++ src/app.js
@@ -1,2 +1,4 @@
+import './style.css';
+
 const app = document.getElementById('app');
 app.textContent = 'Welcome to JS Playground 🎮';
--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,11 @@ module.exports = {
             ],
           ]
         }
-      }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ]
   },
   plugins: [
