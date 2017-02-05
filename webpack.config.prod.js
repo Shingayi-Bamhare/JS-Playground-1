ada2ccd4e51bb950f5d0438899d5fc00a4ddcefb
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2017-02-05 10:50:30 +0000

    Simplify css loader

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -27,12 +27,8 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          {
-            loader: 'style-loader',
-          },
-          {
-            loader: 'css-loader',
-          }
+          'style-loader',
+          'css-loader'
         ]
       },
       {
